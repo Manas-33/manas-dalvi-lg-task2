deb13ad0a9cef91c6c335d855c863a5091b09c5b
Manas Dalvi <dalvimanas33@gmail.com>
2024-01-29 09:54:09 +0530

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:manas_dalvi_task2/screens/homepage.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +26,40 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        home: AnimatedSplashScreen(
+          nextScreen: HomePage(),
+          splash: Column(
+            children: [
+              Image.asset(
+                'assets/images/image.png',
+                scale: 3,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Liquid Galaxy Controller",
+                style: GoogleFonts.openSans(
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: .5,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              )
+            ],
+          ),
+          duration: 1700,
+          splashTransition: SplashTransition.scaleTransition,
+          pageTransitionType: PageTransitionType.leftToRight,
+        ),
+      ),
+    );
   }
 }
